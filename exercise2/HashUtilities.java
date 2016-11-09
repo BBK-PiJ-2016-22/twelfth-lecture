9febc2a36400697670459fda2a3f978c06891588
Mr Casper Nielsen <cniels02@dcs.bbk.ac.uk>
2016-11-09 15:54:39 +0000

    first commit

---
+++ exercise2/HashUtilities.java
@@ -0,0 +1,22 @@
+class HashUtilities{
+	
+	public static int shortHash(int number){
+			
+		int number2 = number % 1000;
+	
+		return Math.abs(number2);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
